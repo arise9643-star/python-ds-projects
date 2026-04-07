4fccd402c496e3340750c3a84b279830c75b222e
arise9643-star <arise9643@gmail.com>
2026-04-07 23:05:48 +0300

    plotly updates

--- plotl.py
+++ plotl.py
@@ -1,6 +1,12 @@
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 df = pd.read_csv("train.csv")
-survival_count = df["Survived"].value_counts().reset_index()
-fig = px.bar(survival_count, x = "Survived",  y = "count" , title= "Survival")
+fig = px.scatter(df ,x="Age",y="Fare",color="Survived",title="Age vs Fare")
 fig.show()
+hist = px.histogram(df,x="Age",title="age distribution")
+hist.show()
+survived_rate = df["Survived"].value_counts().reset_index()
+bar = px.bar(survived_rate,x="Survived",title="survived")
+bar.show()
+box = px.box(df,x="Pclass",y="Age",title="pclass vs age")
+box.show()
